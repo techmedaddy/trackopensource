6a05f35027fd60b919793bd5caa7952b57b8fc00
techmedaddy <umarejazimam69@gmail.com>
2026-06-05 21:50:04 +0530

    Added a comment to test deployment

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -68,3 +68,4 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+// small change to trigger a new deployment
